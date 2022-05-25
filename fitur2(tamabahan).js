531b4b0d9ea455f52e1289b0e943c95543377225
randis wahyuni <202020646@mhs.udb.ac.id>
2022-05-25 18:48:59 +0700

    fitur tambahn

---
+++ fitur2(tamabahan).js
@@ -0,0 +1,16 @@
+
+		function showPass(){
+			var login= false
+		while (login == false) {
+		
+			 var sandi = prompt ("Masukan Password Anda")
+			 if (sandi=="randis"){
+				 login = true
+				 alert("Password anda benar")
+			 }
+			 else {
+			 alert ("Password anda salah")}
+			
+			}
+		}
+		showPass(); 
